ae022ca0972287376e4b0341ac82e16e2040406b
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-05 23:01:03 +0600

    navbar & banner and 3 collam completed

--- src/Pages/Shared/Navbar/Header/Header.jsx
+++ src/Pages/Shared/Navbar/Header/Header.jsx
@@ -4,8 +4,8 @@ import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='text-center'>
+            <img className='mx-auto my-10' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
             <p className='text-xl'>Sunday, November 27, 2022</p>
             <p className='text-xl'>{moment().format("dddd,MMMM D, YYYY")}</p>
